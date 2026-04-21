da2b7b780e709ade105e8e77ae33315181acad36
Fujimatsu <c0c250649c@edu.teu.ac.jp>
2026-04-21 14:22:48 +0900

    練習０２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,10 +1,11 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
-DELTA = {pg.K_UP: (0, -5),  # 練習問題01
+DELTA = {pg.K_UP: (0, -5),  # 練習問題01 / キーと移動量の辞書を作成
          pg.K_DOWN: (0, +5),
          pg.K_LEFT: (-5, 0),
          pg.K_RIGHT: (+5, 0)}
@@ -18,6 +19,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20))   # 練習問題02 / 爆弾を描画
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -28,12 +36,19 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        for k, mv in DELTA.items(): # 練習問題01
+
+        for k, mv in DELTA.items(): # 練習問題01 / 移動量を加算
             if key_lst[k]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+
+        screen.blit(bb_img, bb_rct) # 練習問題02 / 爆弾表示
+        vx, vy = 5, 5
+        bb_rct.move_ip(vx, vy) 
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
